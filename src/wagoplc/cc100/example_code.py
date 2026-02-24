3b99a39ef8184524f4c4a48746e09bcaaf1fe3f6
Kilian Romberg <kilian.romberg@wago.com>
2026-02-24 12:10:35 +0100

    Refine example code

--- src/wagoplc/cc100/example_code.py
+++ src/wagoplc/cc100/example_code.py
@@ -1,16 +1,55 @@
-from wagoplc import main,Task,DigitalInput,DigitalOutput
+# Library code
 
-task = Task()
+class PLC:
+     
+     def __init__(self):
+          self.tasks = {}
+     
+     def setup(self):
+          """Get user variables."""
+          def decorator_setup(func):
+               ...
+     
+     def task(
+               self, _func: function = None, *,
+               priority: int = 15, watchdog: int = 400000,
+               cycletime: int = 50):
+          """Register task."""
+          def decorator_task(func):
+               ...
 
-@task.setup
+class Task:
+     
+     def __init__(self):
+          self.cycle_func = None
+          self.user_vars = {}
+          self.cc_obj = None
+     
+     def cycle(self):
+          """Run one loop cycle."""
+          input_image = self.cc_obj.read_inputs(self.user_vars)
+          output_image = self.cycle_func(**input_image)
+          self.cc_obj.write_outputs(output_image)
+
+# --------------------------------------------------
+# plc_prg.py
+
+from wagoplc import main, PLC, DigitalInput, DigitalOutput
+
+plc = PLC()
+
+@plc.setup
 def setup():
-    variables = {"xEndlageS1": DigitalInput(1),"xLuefter": DigitalOutput(1)}
-    return variables
+    xEndlageS1 = DigitalInput(1)
+    xLuefter = DigitalOutput(1)
 
-@task(cycletime=5)
+    return locals()
+
+@plc.task(cycletime=5)
 def loop(xEndlageS1):
         if xEndlageS1:
             xLuefter = True
+
         return locals()
 
 if __name__ == "__main__":
