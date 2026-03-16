3060e4aa52d33d94fde9e57cd52f412dba5a1db4
Johann TIMM <Johann.Timm@wago.com>
2026-03-16 16:06:53 +0100

    remove analog inputs cc100 9403

--- src/wagoplc/cc100/cc100_9403.py
+++ src/wagoplc/cc100/cc100_9403.py
@@ -2,6 +2,7 @@ from wagoplc.cc100.cc100_v1 import CC100_v1
 
 class CC100_9403(CC100_v1):
     DIN = "/sys/devices/platform/soc/44009000.spi/spi_master/spi1/spi1.0/din"
-
+    IN_VOLTAGE0_RAW = ""
+    IN_VOLTAGE3_RAW = ""
     def __init__(self):
         super().__init__()
